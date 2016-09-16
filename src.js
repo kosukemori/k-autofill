e3eef1f47a87edba99a06ff68ddf78d2dd22d559
kosmori <kosmori@cisco.com>
2016-09-15 17:47:54 -0700

    Fix the PAY CATEGORY proglem

--- src.js
+++ src.js
@@ -2,11 +2,6 @@
   s = document.createElement("script");
   s.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js";
   s.type = 'text/javascript';
-  /*
-  s.onload = function() {
-    f(jQuery.noConflict(true))
-  };
-  */
   document.body.appendChild(s)
   var checkReady = function(callback) {
     if (window.jQuery) {
@@ -17,54 +12,74 @@
     }
   };
   checkReady(function($){
-    var time = 0;
-    var timer = setInterval(function() {
-      // console.log('while');
+    var timer = null;
+    var arr = [];
+    function fill() {
+      var time = 0;
+      for (var i = 0; i < arr.length; i++) {
+        if (i % 2 == 0) {
+          $($(arr[i]).find('.clockLeft input')).attr('value', '9:00a');
+          $($(arr[i]).find('.clockRight input')).attr('value', '12:00p');
+        } else {
+          $($(arr[i]).find('.clockLeft input')).attr('value', '1:00p');
+          $($(arr[i]).find('.clockRight input')).attr('value', '6:00p');
+        }
+        (function(i) {
+          setTimeout(function() {
+            $($(arr[i]).find('.icon-List_18')[1]).click();
+          }, time);
+        }(i));
+        time += 2000;
+        (function(i) {
+          setTimeout(function() {
+            $($('.icon-Flag_16', top.frames['ADMIN_CENTER'].frames['PopupBodyFrame'].document.body)[3]).parent().click();
+            if (i == (arr.length - 1)) {
+              alert("Autofill is finished");
+            }
+          }, time);
+        }(i));
+        time += 2000;
+      }
+    }
+    function add_row() {
       var trs = $('table.reportTable > tbody > tr', top.frames['ADMIN_CENTER'].document.body);
       var i = 0;
+      arr = []; // Use only last loop's one
       for (i = 0; i < trs.length; i++) {
         // Skip without firstRow
         if ($(trs[i]).attr("class") == null || $(trs[i]).attr("class").match(/firstRow/) == null) {
           continue;
         }
-        // console.log($($(trs[i]).find('.dateLabel > div')).text());
         if ($($(trs[i]).find('.dateLabel > div')).text().match(/Sat|Sun/) != null) {
           continue;
         }
-        // $($(trs[i]).find('.icon-List_18')[1]).click();
         // Check an index of next dayTotalRow
         var tmp = i;
         while ($(trs[tmp]).attr("class") != null && $(trs[tmp]).attr("class").match(/dayTotalRow/) == null) {
           tmp += 1;
         }
-        // console.log(i);
-        // if this day (trs[i]) is not edited, update the timesheet
+        //console.log(i);
+        // This day (trs[i]) is not edited
         if ($($(trs[tmp]).find('input')[1]).attr('value') == '0:00') {
           // console.log('this days is not edited:', i);
           if ($(trs[i + 1]).attr("class").match(/middleRow/) == null) {
-            // console.log('click', i);
             $($(trs[i]).find('a[title="Add Row"]')).click();
             break;
           } else {
-            // console.log('input', i);
-            $($(trs[i]).find('.clockLeft input')).attr('value', '9:00a');
-            $($(trs[i]).find('.clockRight input')).attr('value', '12:00p');
-            $($(trs[i + 1]).find('.clockLeft input')).attr('value', '1:00p');
-            $($(trs[i + 1]).find('.clockRight input')).attr('value', '6:00p');
-
-            $($(trs[i]).find('.dropdown_toggle')[1]).removeClass('empty_field');
-            $($(trs[i]).find('.dropdown_toggle')[1]).attr('title', 'Regular');
-            $($($(trs[i]).find('.dropdown_toggle')[1]).find('.dropdown_toggle_text')).text('Regular');
-            $($(trs[i + 1]).find('.dropdown_toggle')[1]).removeClass('empty_field');
-            $($(trs[i + 1]).find('.dropdown_toggle')[1]).attr('title', 'Regular');
-            $($($(trs[i + 1]).find('.dropdown_toggle')[1]).find('.dropdown_toggle_text')).text('Regular');
+            arr.push(trs[i]);
+            arr.push(trs[i + 1]);
           }
         }
       }
-      if (i == trs.length) {
+      if (i == trs.length && timer != null) {
         clearInterval(timer);
-        alert('Autofill is finished');
+        fill();
+        // alert('Autofill is finished');
       }
-    }, 2000);
+    }
+    add_row();
+    timer = setInterval(function() {
+      add_row();
+    }, 2200);
   });
 })();
