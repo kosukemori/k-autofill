1b409b2f73247e257f86f20dda4ac21e24735b6e
kosmori <kosmori@users.noreply.github.com>
2017-01-03 10:58:28 -0800

    Change current-timesheet.js for new KRONOS

--- dist/current-timesheet.js
+++ dist/current-timesheet.js
@@ -14,17 +14,25 @@ function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, a
       try {
         const $a = $('#TopMenu_HM_Menu1', top.frames[0].document.body);
         $a.click();
-      } catch (e) {/* Do nothing */}
+      } catch (e) {
+        console.log(e);
+      }
       yield sleep(500);
       try {
-        const $b = $('#HM_Item1_12', top.frames[1].document.body);
-        $b.click();
-      } catch (e) {/* Do nothing */}
+        // const $b = $('#HM_Item1_12', top.frames[1].document.body);
+        const $b = $('span:contains("My Timesheet")');
+        console.log($b);
+        // $b.click();
+      } catch (e) {
+        console.log(e);
+      }
       yield sleep(500);
       try {
-        const $c = $('#HM_Item1_12_1 .itemText', top.frames[1].document.body);
-        $c.click();
-      } catch (e) {/* Do nothing */}
+        // const $c = $('#HM_Item1_12_1 .itemText', top.frames[1].document.body);
+        // $c.click();
+      } catch (e) {
+        console.log(e);
+      }
     });
 
     return function main() {
